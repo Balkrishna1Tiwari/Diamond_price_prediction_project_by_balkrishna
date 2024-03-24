42e55a78b7986c868c0f595e67fbba08d9dcdb3b
Balkrishna1Tiwari <balkrishnatiwari389@gmail.com>
2024-03-24 10:48:28 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+from typing import List
+
+def get_requirements(file_path: str) -> List[str]:
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+        return requirements
+
+setup(
+    name='Diamond_price_prediction',
+    version='0.0.1',
+    author='Blakrishna Tiwari',
+    author_email='balkrishnatiwari389@gmail.com/bt3277298@gmail.com',
+    install_requires=get_requirements('requirements.txt'),
+    packages=find_packages()
+)
+
+
+
